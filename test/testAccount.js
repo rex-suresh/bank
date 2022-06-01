1f1d2808df6a8b823cd7b65c680c7b6d285b051f
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-06-01 09:45:40 +0530

    Added test for balance and deduct

--- test/testAccount.js
+++ test/testAccount.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const Account = require('./Account.js');
+const {Account} = require('../src/Account.js');
 
 describe('Account', () => {
   it( 'Should add amount,(change amount state), when added', () => {
@@ -9,5 +9,19 @@ describe('Account', () => {
 
     assert.ok(account.equals(otherAccount) && otherAccount.equals(account));
     assert.deepStrictEqual(report, {credit: 200, balance: 200});
+  });
+
+  it( 'Should reduce amount,(change amount state), when deducted', () => {
+    const account = new Account('suresh', 13, 1000);
+    const otherAccount = new Account('suresh', 13, 900);
+    const report = account.deductAmount(100);
+
+    assert.ok(account.equals(otherAccount) && otherAccount.equals(account));
+    assert.deepStrictEqual(report, {debit: 100, balance: 900});
+  });
+
+  it( 'Should return balance when balance is requested', () => {
+    const account = new Account('suresh', 12, 800);
+    assert.deepStrictEqual(account.accountBalance(), {balance: 800});
   });
 });
